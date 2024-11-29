74819102a6ff643a099c14df3e3d35fbb5f4bdd1
abu_sulaymaan <rustamov.abdurasul@inbox.ru>
2024-11-29 20:28:19 +0500

    Initial commit

---
+++ example.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+// What is Version control
+// Installing git
+// Git Workflow init, add, commit 
+// git states -> git status
+// GitHub -> creating repos, cloning, 
+// Branches -> git branch, git checkout
+// Pulling and merge conflicts
+// gitignore
